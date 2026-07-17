d65ccb30fdf27eef10e57397527cd1d6769f0e21
makeloftergreat <03laozu@gmail.com>
2026-07-17 10:44:35 +0800

    update: lib/supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY || process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "https://ncvhslfqibhljwugqnww.supabase.co";
+const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY || process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "sb_publishable_UCDr_KBV6c1iWNiqU9h5OQ_RU6CHhYa";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
